edf4066de8088d1e5cf0bf84b3e918f26184683f
zroex <shuhungchu@yile.com.tw>
2022-12-10 15:38:25 +0800

    firstcommit

---
+++ BudgetSenice/BudgetSenice/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "BudgetSenice" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "BudgetSenice" )]
+[assembly: AssemblyCopyright( "Copyright ©  2022" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+
+[assembly: Guid( "fff34d0a-6b16-4d63-9702-9fced03ee96c" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
